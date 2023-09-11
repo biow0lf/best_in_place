d029b11763bc3abde430cb3546b5915be1bc5d41
Igor Zubkov <igor.zubkov@gmail.com>
2023-09-11 14:50:22 +0200

    Ruby 3 fixes

--- lib/best_in_place/display_methods.rb
+++ lib/best_in_place/display_methods.rb
@@ -5,19 +5,19 @@ module BestInPlace
     class Renderer < Struct.new(:opts)
       def render_json(object)
         case opts[:type]
-          when :model
-            { display_as: object.send(opts[:method]) }.to_json
-          when :helper
-            value = if opts[:helper_options]
-                      BestInPlace::ViewHelpers.send(opts[:method], object.send(opts[:attr]), opts[:helper_options])
-                    else
-                      BestInPlace::ViewHelpers.send(opts[:method], object.send(opts[:attr]))
-                    end
-            { display_as: value }.to_json
-          when :proc
-            { display_as: opts[:proc].call(object.send(opts[:attr])) }.to_json
-          else
-            '{}'
+        when :model
+          { display_as: object.send(opts[:method]) }.to_json
+        when :helper
+          value = if opts[:helper_options]
+                    BestInPlace::ViewHelpers.send(opts[:method], object.send(opts[:attr]), opts[:helper_options])
+                  else
+                    BestInPlace::ViewHelpers.send(opts[:method], object.send(opts[:attr]))
+                  end
+          { display_as: value }.to_json
+        when :proc
+          { display_as: opts[:proc].call(object.send(opts[:attr])) }.to_json
+        else
+          '{}'
         end
       end
     end
@@ -30,15 +30,15 @@ module BestInPlace
     end
 
     def add_model_method(klass, attr, display_as)
-      model_attributes(klass)[attr.to_s] = Renderer.new method: display_as.to_sym, type: :model
+      model_attributes(klass)[attr.to_s] = Renderer.new({method: display_as.to_sym, type: :model})
     end
 
     def add_helper_method(klass, attr, helper_method, helper_options = nil)
-      model_attributes(klass)[attr.to_s] = Renderer.new method: helper_method.to_sym, type: :helper, attr: attr, helper_options: helper_options
+      model_attributes(klass)[attr.to_s] = Renderer.new({method: helper_method.to_sym, type: :helper, attr: attr, helper_options: helper_options})
     end
 
     def add_helper_proc(klass, attr, helper_proc)
-      model_attributes(klass)[attr.to_s] = Renderer.new type: :proc, attr: attr, proc: helper_proc
+      model_attributes(klass)[attr.to_s] = Renderer.new({type: :proc, attr: attr, proc: helper_proc})
     end
 
     def model_attributes(klass)
